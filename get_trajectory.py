2757ea9976fd12917b5d1cc4b030e72551da1ac3
Cian Kelly <ciank@ntnu.no>
2024-02-01 14:35:29 +0100

    Intermediate file for storing polygon values in grid cells and then melding with trajectory information

--- get_trajectory.py
+++ get_trajectory.py
@@ -1,11 +1,15 @@
 import netCDF4 as nc
 from netCDF4 import num2date
+from netCDF4 import Dataset    # Note: python is case-sensitive!
 import numpy as np
 import math
 import shapely.geometry
 import pandas as pd
 import geopandas as gpd
 import pickle
+import os
+
+# from Krillmod.import_settings import sv_dir
 
 
 def store_traj(file):
@@ -49,37 +53,89 @@ def single_traj(store, idx):
     return slice_t
 
 
+def read_regions():
+    from Krillmod.import_settings import tr_dir
+    r_save = tr_dir + 'regions.nc'
+    nc_file = nc.Dataset(r_save, mode='r', format='NETCDF4_CLASSIC')
+    nc_file.variables.keys()
+    in_polt = nc_file.variables["in_region"][:]
+    # store_pol = dict([("in_reg", in_polt)])
+    nc_file.close()
+    # Make a check for the year I am trying to read;
+    return in_polt
+
+
 def get_regions(store, shape_v):
-    # Note: Use slices to index lists
+
+    # Shapes from trajectory file for creating nc dimensions
     shp_p = np.shape(shape_v)  # Number of polygons
     shp_t = np.shape(store['time'])  # Time steps
-    for j in range(0, shp_p[0]):
-        pol_idx = j
-        str_v = ('poly' + str(pol_idx))
-        if j == 0:
-            list_v = {str_v: 0}# list_v = dict([(str_v, 0)])
-        else:
-            list_v[str_v] = 0
+    shp_i = np.shape(store['xp'])  # Number of individuals
 
-    for j in range(0, shp_p[0]): #-1):
-        pol_idx = j  # Index current polygon
-        poly1 = shape_v[pol_idx:pol_idx + 1].geometry  # Extract polygon geometry (gpd object)
-        str_v = ('poly' + str(pol_idx))
-        for ii in range(0, shp_t[0]): #shp_t[0]):
-            idx = ii  # Current time
-            store_t = single_traj(store, idx)  # Time slice of trajectory
-            x = store_t['xp']
-            y = store_t['yp']
-            p_id = inpoly(x, y, poly1)
-            if ii == 0:
-                # list_v[str_v] = [p_id]
-                list_v[str_v] = [p_id]
-            else:
-                list_v[str_v].append(p_id)
-            # list_v[str_v] = [list_v[str_v], p_id]
-        # ['Polygon ' + str(pol_idx)]
+    iv = np.shape(store['depth'])
 
-    return list_v
+    from Krillmod.import_settings import shp_dir
+    file_inter = shp_dir + str('poly2grid.npy')
+    if not os.path.exists(file_inter):
+        print('Note: Creating intermediate .npy file with polygon indices mapped to grid coordinates')
+        store_ids = np.zeros([iv[0]*iv[1], 2])
+        store_poly = np.zeros([iv[0]*iv[1], 1])
+        c = 0
+        for i in range(0, iv[0]):
+            for j in range(0, iv[1]):
+                store_ids[c, 0] = j
+                store_ids[c, 1] = i
+                c = c + 1
+
+        for p in range(0, shp_p[0]):
+            print('poly = ' + str(p+1) + ' of ' + str(shp_p[0]))
+            poly1 = shape_v[p:p + 1].geometry  # Extract polygon geometry (gpd object)
+            p_id = inpoly(store_ids[:, 0], store_ids[:, 1], poly1)
+            store_poly[p_id == 1] = p + 1
+        poly_ids = np.reshape(store_poly, [iv[0], iv[1]])
+        np.save(file_inter, poly_ids)
+
+    poly_ids = np.load(file_inter)
+    temp_poly = np.empty([shp_t[0], shp_i[1]])
+    for t in range(0, shp_t[0]):
+        store_t = single_traj(store, t)  # Time slice of trajectory
+        x = store_t['xp'].astype(int)
+        y = store_t['yp'].astype(int)
+        poly_fid = poly_ids[y, x]
+        idx_pid = (np.where(poly_fid > 0))
+        temp_poly[t, idx_pid] = poly_fid[idx_pid]
+        print('t = ' + str(t) + ' of ' + str(shp_t[0]) + ' steps')
+        print('Percent complete = ' + str(np.ceil((t/shp_t[0])*100)))
+
+    # Check that file isn't already open
+    try:
+        nc_file.close()  # just to be safe, make sure dataset is not already open.
+    except:
+        pass
+
+    # Create filepath
+    from Krillmod.import_settings import sv_dir, tr_dir
+    # r_save = sv_dir + 'regions.nc'
+    r_save = tr_dir + 'regions.nc'
+    nc_file = Dataset(r_save, mode='w', format='NETCDF4_CLASSIC')
+
+    # Specify nc dimensions
+    part_dim = nc_file.createDimension('particle', shp_i[1])
+    time_dim = nc_file.createDimension('time', shp_t[0])
+    area_dim = nc_file.createDimension('polygon', shp_p[0])
+
+    # for dim in nc_file.dimensions.items():
+    #     print(dim)
+
+    # Create variable for storing presence/ absence in each region at each time:
+    in_region = nc_file.createVariable('in_region', np.int32, ('time', 'particle'))  # , 'polygon'))
+    # consider adding variables: active part; time; polygon names;
+
+    in_region[:] = temp_poly
+    print(nc_file)
+    nc_file.close()
+    print('Dataset is closed!')
+    return
 
 
 def inpoly(x, y, poly1):
@@ -90,7 +146,8 @@ def inpoly(x, y, poly1):
     gdf.crs = polya.crs
     in_pol = gpd.tools.sjoin(gdf, polya, predicate="within", how='left')
     in_idx = np.where(in_pol.index_right > -1)
-    p_id = in_idx[0]
+    p_id = np.zeros(np.shape(x))
+    p_id[in_idx[0]] = 1
     return p_id
 
 
--- krillmod.py
+++ krillmod.py
@@ -2,19 +2,41 @@
 from Krillmod.import_settings import *
 
 # Trajectory data in dictionary format
-store = store_traj(tr_file)    # Read trajectory file
-shape_v = read_ssmu(shp_file)  # Read ssmu shapefiles
+# store = store_traj(tr_file)    # Read trajectory file
+# shape_v = read_ssmu(shp_file)  # Read ssmu shapefiles
+
+poly_id = read_regions()
+new_mat = np.zeros(np.shape(poly_id))
 
 
-list_v = get_regions(store, shape_v)
-np.save('myfile.npy', list_v)
-np.load('myfile.npy',allow_pickle='TRUE')
+new_mat[poly_id == 1] = 1
+new_mat[poly_id == 2] = 1
+
+
+ps = poly_id[0, :]
+poly_np = [1, 2]
+shp_p = np.shape(poly_np)
+shp_t = np.shape(poly_id)
 
 
 
-df = dom_path(store)
-plot_dom_path(df, store)
-sim_account(store)
+for t in range(0, shp_t[0]):
+    for i in range(0, shp_p[0]):
+        region_id[region_id == 1] = 1
+
+
+#get_regions(store, shape_v)
+
+
+
+# np.save('myfile.npy', list_v)
+# np.load('myfile.npy',allow_pickle='TRUE')
+
+
+
+#df = dom_path(store)
+#plot_dom_path(df, store)
+#sim_account(store)
 # Save the array to a binary file
 #file = 'C:/Users/ciank/PycharmProjects/sinmod/Krillmod/results/dom_paths.npy'
 #np.save(file, np.array(df))
@@ -26,21 +48,6 @@ sim_account(store)
 
 
 # First index should be for
-
-
-list_v = ["R1", "R2"]
-foo = {}
-foo['R1'][0] = lat1
-
-idx = 0
-
-
-
-
-
-
-
-point1 = lon[0:10], lat[0:10]
 
 
 
