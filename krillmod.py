a69fc29d45bd4a9238b39227293e3cc0a60e80de
Cian Kelly <ciank@ntnu.no>
2024-02-19 19:19:43 +0100

    Added check for empty vectors

--- krillmod.py
+++ krillmod.py
@@ -1,15 +1,15 @@
 # master file for krill model analysis
 from import_settings import locate_folders
-#from analyse_trajectory import particle_visits
+from analyse_trajectory import particle_visits
 
 # Reformat trajectory data and setup target folders:
 shape_name = 'ssmusPolygon.shp'
-sim_folder = '2016'
+sim_folder = '2018'
 comp_node = 'local'
 list_dir = locate_folders(comp_node, sim_folder, shape_name)
 
 # Analyse trajectories
-# particle_visits(list_dir)
+particle_visits(list_dir)
 
 
 
