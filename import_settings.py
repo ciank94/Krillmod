b5bd89ef4f7fccbd7bc9b9a5ad762ca6e5681aed
Cian Kelly <ciank@ntnu.no>
2024-02-19 18:45:48 +0100

    Code modified for functioning with HPC

--- import_settings.py
+++ import_settings.py
@@ -3,48 +3,29 @@ import os
 import sys
 
 
-def locate_folders():
-    comp_node = 'local'
-
+def locate_folders(comp_node, sim_folder, shape_name):
     # Name directories here
     if comp_node == 'local':
         shape_folder = 'C:/Users/ciank/PycharmProjects/sinmod/Krillmod/ssmu/'  # Directory where the shape files are stored
         traj_folder = 'A:/Cian_sinmod/meeso_sim/sim_'  # Directory where the trajectory file is stored
         save_folder = 'C:/Users/ciank/PycharmProjects/sinmod/Krillmod/results/'  # Directory where data will be saved
-        sim_folder = '2017'
-        shape_name = 'ssmusPolygon.shp'
-        dir_name = init_folders(sim_folder, shape_name, shape_folder, save_folder, traj_folder)
+        list_dir = init_folders(comp_node, sim_folder, shape_name, shape_folder, save_folder, traj_folder)
     elif comp_node == 'saga':
         shape_folder = '/cluster/projects/nn9828k/Cian_sinmod/python/Krillmod/ssmu/'  # Directory where the shape files are stored
         traj_folder = '/cluster/projects/nn9828k/Cian_sinmod/meeso_sim/sim_'  # Directory where the trajectory file is stored
         save_folder = '/cluster/projects/nn9828k/Cian_sinmod/python/Krillmod/results/'  # Directory where data will be saved
-        sim_folder = '2016'
-        shape_name = 'ssmusPolygon.shp'
-        dir_name = init_folders(sim_folder, shape_name, shape_folder, save_folder, traj_folder)
+        list_dir = init_folders(comp_node, sim_folder, shape_name, shape_folder, save_folder, traj_folder)
     else:
         print('Error: computation node not specified')
-        dir_name = []
-
-    list_dir = dir_name
-
+        sys.exit()
     return list_dir
 
 
-def init_folders(sim_folder, shape_name, shape_folder, save_folder, traj_folder):
+def init_folders(comp_node, sim_folder, shape_name, shape_folder, save_folder, traj_folder):
     # Stores directories in a dictionary
-    list_dir = store_folders(sim_folder, shape_name, shape_folder, save_folder, traj_folder)
-    list_dir = store_files(list_dir)
-    return list_dir
-
-
-def store_folders(sim_folder, shape_name, shape_folder, save_folder, traj_folder):
-    # Stores relevant folders in dictionary
     list_dir = dict([('shape_folder', shape_folder), ('traj_folder', traj_folder), ('save_folder', save_folder),
-                    ('sim_folder', sim_folder), ('shape_name', shape_name)])
-    return list_dir
+                     ('sim_folder', sim_folder), ('shape_name', shape_name)])
 
-
-def store_files(list_dir):
     # Adds relevant files to dictionary. The regions.nc file is derived from trajectory.nc and shape files,
     # storing information on polygon index of individual for each time step
     list_dir['shape_file'] = list_dir['shape_folder'] + list_dir['shape_name']
@@ -62,7 +43,11 @@ def store_files(list_dir):
         sys.exit()
 
     if not os.path.exists(list_dir['save_folder'] + list_dir['sim_folder']):
-        os.mkdir(list_dir['save_folder'] + list_dir['sim_folder'])
+        if comp_node == 'local':
+            os.mkdir(list_dir['save_folder'] + list_dir['sim_folder'])
+        else:
+            os.system('mkdir' + list_dir['save_folder'])
+            os.system('mkdir' + list_dir['save_folder'] + list_dir['sim_folder'])
 
     if not os.path.exists(list_dir['time_file']):
         get_times(list_dir)  # Creates time file in folder
@@ -70,8 +55,11 @@ def store_files(list_dir):
     if not os.path.exists(list_dir['depth_file']):
         get_depth(list_dir)  # Creates depth file in folder
 
-    if not os.path.exists(list_dir['reg_file']):
+    if not (os.path.exists(list_dir['reg_file'])):
+        # Use try os.system(echo) or something of the sort  here;
         print('Note: Reformatting trajectory data for analysis')
         store_regions(list_dir)  # Creates intermediate file with trajectory and regional data
 
     return list_dir
+
+
